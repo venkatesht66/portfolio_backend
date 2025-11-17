9782253a599489a1389b34bdfd0822be2957af8f
venkateshfswb <tammisettyvenkatesh@Venkys-MacBook-Air.local>
2025-11-17 12:08:59 +0530

    profile updated

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const AdminController = require('../controllers/adminController');
 
-router.get('/profile', auth, AdminController.getProfile);
+router.get('/profile', AdminController.getProfile);
 router.put('/profile', auth, AdminController.updateProfile);
 
 module.exports = router;
