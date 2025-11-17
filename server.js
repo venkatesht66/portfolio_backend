7de4fec06e78cab2b32c28bddbf56fc84f9fdff8
venkateshfswb <tammisettyvenkatesh@Venkys-MacBook-Air.local>
2025-11-17 10:41:07 +0530

    cors added

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const certificationRoutes = require('./routes/certificationRoutes');
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://portfolio-snowy-eight-curyi5ea91.vercel.app/"], 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
